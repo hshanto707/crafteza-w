4e7fa8efbed248e089295149f2f35cc8d52dda69
Hasibul Hasan <hshanto707@gmail.com>
2022-12-24 00:48:15 +0600

    project initialized

---
+++ app.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <p class="shadow-sm text-2xl">Hello</p>
+    <p class="">Hello</p>
+  </div>
+</template>
---
+++ layouts/default.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <p class="shadow-sm text-2xl">Hello</p>
+  </div>
+</template>
