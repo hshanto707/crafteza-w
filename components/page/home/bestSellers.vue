6275daf487189242c008f3de1c72f91c3c5fc51a
Hasibul Hasan <hshanto707@gmail.com>
2022-12-30 02:51:21 +0600

    best sellers added

--- components/page/home/bestSellers.vue
+++ components/page/home/bestSellers.vue
@@ -4,6 +4,60 @@
       <p class="sacramento text-5xl mb-4 text-gray-900">Best Sellers</p>
       <p class="twentieth text-4xl text-gray-900">Back In Stock</p>
     </div>
+
+    <div class="flex gap-6 mt-6">
+      <div class="product-card py-6 px-8">
+        <div class="cursor-pointer">
+          <img src="@/assets/images/products/plywood-table.webp" class="mx-auto" alt="">
+          <p class="twentieth text-3xl mt-5">Plywood Table</p>
+          <p class="twentieth text-xl ">$4200</p>
+        </div>
+
+        <div class="flex gap-3 mt-5">
+          <img src="@/assets/icons/favorite.png" class="w-6 cursor-pointer" alt="">
+          <img src="@/assets/icons/cart.png" class="w-6 cursor-pointer" alt="">
+        </div>
+      </div>
+
+      <div class="product-card py-6 px-8">
+        <div class="cursor-pointer">
+          <img src="@/assets/images/products/l-shape-sofa.webp" class="mx-auto" alt="">
+          <p class="twentieth text-3xl mt-5">Plywood Table</p>
+          <p class="twentieth text-xl ">$4200</p>
+        </div>
+
+        <div class="flex gap-3 mt-5">
+          <img src="@/assets/icons/favorite.png" class="w-6 cursor-pointer" alt="">
+          <img src="@/assets/icons/cart.png" class="w-6 cursor-pointer" alt="">
+        </div>
+      </div>
+      
+      <div class="product-card py-6 px-8">
+        <div class="cursor-pointer">
+          <img src="@/assets/images/products/standard-sofa.webp" class="mx-auto" alt="">
+          <p class="twentieth text-3xl mt-5">Plywood Table</p>
+          <p class="twentieth text-xl ">$4200</p>
+        </div>
+        
+        <div class="flex gap-3 mt-5">
+          <img src="@/assets/icons/favorite.png" class="w-6 cursor-pointer" alt="">
+          <img src="@/assets/icons/cart.png" class="w-6 cursor-pointer" alt="">
+        </div>
+      </div>
+
+      <div class="product-card py-6 px-8">
+        <div class="cursor-pointer">
+          <img src="@/assets/images/products/german-cotton-chair.webp" class="mx-auto" alt="">
+          <p class="twentieth text-3xl mt-5">Plywood Table</p>
+          <p class="twentieth text-xl ">$4200</p>
+        </div>
+        
+        <div class="flex gap-3 mt-5">
+          <img src="@/assets/icons/favorite.png" class="w-6 cursor-pointer" alt="">
+          <img src="@/assets/icons/cart.png" class="w-6 cursor-pointer" alt="">
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
